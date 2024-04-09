e29d8b44807daebfcea9d8ea5f82fc095cd4d260
Francesco Ruta <francescoruta.99@gmail.com>
2024-04-09 15:31:27 +0200

    Dynfnres

--- dirpc/src/dyn_fn.rs
+++ dirpc/src/dyn_fn.rs
@@ -8,7 +8,7 @@ pub trait IntoDynFunction<Context, RequestState, PhantomGeneric> {
     fn get_type_description(names: Self::NameTuple) -> FunctionDescription;
 }
 
-impl<Context, RequestState, Fut, R, F> IntoDynFunction<Context, RequestState, (R, )> for F
+impl<Context, RequestState, Fut, R, F> IntoDynFunction<Context, RequestState, ((R, ), )> for F
 where
     Fut: std::future::Future<Output = R> + Send + Sync + 'static,
     R: serde::Serialize + GetTypeDescription,
@@ -20,6 +20,37 @@ where
             let function = self.clone();
             Box::pin(async move {
                 Serializer::serialize(function().await)
+            })
+        })
+    }
+    fn get_type_description(_names: Self::NameTuple) -> FunctionDescription {
+        FunctionDescription {
+            args_types: vec![],
+            return_type: R::get_type_description(),
+        }
+    }
+}
+
+impl<Context, RequestState, Fut, R, E, F> IntoDynFunction<Context, RequestState, ((R, E), )> for F
+where
+    Fut: std::future::Future<Output = Result<R, E>> + Send + Sync + 'static,
+    R: serde::Serialize + GetTypeDescription,
+    E: std::error::Error,
+    F: FnOnce() -> Fut + Clone + Send + Sync + 'static,
+{
+    type NameTuple = ();
+    fn into_dyn_fn<Serializer: RpcSerializer, Deserializer: RpcDeserializer>(self) -> DynFunction<Context, RequestState> {
+        Box::new(move |_ctx, _req| {
+            let function = self.clone();
+            Box::pin(async move {
+                match function().await {
+                    Ok(v) => Serializer::serialize(Ok::<R, String>(v)),
+                    Err(e) => {
+                        let e = e.to_string();
+                        eprintln!("ERROR: {e}");
+                        Serializer::serialize(Err::<R, String>(e))
+                    },
+                }
             })
         })
     }
@@ -52,6 +83,41 @@ macro_rules! dyn_fn_impl {
                     let function = self.clone();
                     Box::pin(async move {
                         Serializer::serialize(function($($t_idx?,)*).await)
+                    })
+                })
+            }
+            fn get_type_description(names: Self::NameTuple) -> FunctionDescription {
+                #[allow(non_snake_case)]
+                let ($($t,)*) = names;
+                FunctionDescription {
+                    args_types: vec![$(($t::EXPORT_DEFINITION, $t.into(), $t::get_type_description()),)*],
+                    return_type: R::get_type_description(),
+                }
+            }
+        }
+        impl<Context, RequestState, $($t,)* Fut, R, E, F, StrType> IntoDynFunction<Context, RequestState, (($($t,)* R, E), StrType)> for F
+        where
+            $($t: $crate::inject::Inject<Context, RequestState> + GetTypeDescription + Send + Sync + 'static,)*
+            Fut: std::future::Future<Output = Result<R, E>> + Send + Sync + 'static,
+            R: serde::Serialize + GetTypeDescription,
+            E: std::error::Error,
+            F: FnOnce($($t,)*) -> Fut + Clone + Send + Sync + 'static,
+            StrType: Into<String>,
+        {
+            type NameTuple = ($(type_as_string!($t),)*);
+            fn into_dyn_fn<Serializer: RpcSerializer, Deserializer: RpcDeserializer>(self) -> DynFunction<Context, RequestState> {
+                Box::new(move |ctx, mut req| {
+                    $(let $t_idx = $t::inject::<Deserializer>(ctx, &mut req);)*
+                    let function = self.clone();
+                    Box::pin(async move {
+                        match function($($t_idx?,)*).await {
+                            Ok(v) => Serializer::serialize(Ok::<R, String>(v)),
+                            Err(e) => {
+                                let e = e.to_string();
+                                eprintln!("ERROR: {e}");
+                                Serializer::serialize(Err::<R, String>(e))
+                            },
+                        }
                     })
                 })
             }
